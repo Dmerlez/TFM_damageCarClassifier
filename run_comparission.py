69d6297982894f9edc9deabb25ec52139093ca24
Dmerlez <davomerlez@gmail.com>
2025-05-13 22:06:44 +0200

    Implement Zero-Shot CLIP Model

--- run_comparission.py
+++ run_comparission.py
@@ -1,5 +1,5 @@
 from src.eval import eval_zero_shot_clip
 from src.infer import infer_zero_shot_clip 
 
-eval_zero_shot_clip.main()
-#infer_zero_shot.main()
+#eval_zero_shot_clip.main()
+infer_zero_shot_clip.main()
