36439c27829fa269dd3791103db70fcba1510860
Dmerlez <davomerlez@gmail.com>
2025-05-21 20:37:55 +0200

    Change run_comparission.py comment

--- run_comparission.py
+++ run_comparission.py
@@ -1,5 +1,5 @@
 from src.backend.eval import eval_zero_shot_clip
 from src.backend.infer import infer_zero_shot_clip
 
-eval_zero_shot_clip.main()
-#infer_zero_shot_clip.main()
+#eval_zero_shot_clip.main()
+infer_zero_shot_clip.main()
